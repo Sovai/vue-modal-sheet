d1c91c3810125b6549972c4070db901f09291b40
KEAN Sovai <kean.sovai@ababank.com>
2022-05-05 17:37:50 +0700

    add padding

--- src/components/TheSheet.vue
+++ src/components/TheSheet.vue
@@ -2,7 +2,7 @@
   <button @click="selectedProp === spring">Spring</button>
   <button @click="selectedProp = keyFrame">Keyframe</button>
 
-  <div class="sheet-wrapper" v-motion="'demo'" ref="sheetRef">
+  <div class="sheet-wrapper" ref="sheetRef">
     <div class="bar"></div>
     <h2>Sheet Title</h2>
     <div class="sheet-content">
@@ -67,7 +67,7 @@ let sheetContent = ref(0);
 let state = ref();
 let axisY = ref(0);
 const DRAG_BAR_HEIGHT = 100;
-// const BOTTOM_PADDING = 500;
+const BOTTOM_PADDING = 500;
 
 let windowHeight = ref(0);
 
@@ -115,10 +115,7 @@ function calculateHeight() {
 
     // set sheet to hidden
     axisY.value = windowHeight.value - DRAG_BAR_HEIGHT;
-    push("y", axisY.value, motionProperties, {
-      type: "spring",
-      bounce: 0,
-    });
+    push("y", axisY.value, motionProperties, keyFrame);
 
     // set({
     //   x: 0,
@@ -131,14 +128,16 @@ function handleDrag(ctx) {
     movement: [x, y],
   } = ctx;
   let setY = axisY.value + y;
+
   console.log({
-    y,
-    axisY: axisY.value,
-    setY,
+    // y,
+    // axisY: axisY.value,
+    setY: setY - sheetContent.value,
     sheetContent: sheetContent.value,
-    windowHeight: windowHeight.value,
+    // windowHeight: windowHeight.value,
   });
   // if (setY < sheetContent.value) return;
+
   push("y", setY, motionProperties, selectedProp.value);
 
   // set({
@@ -182,7 +181,7 @@ function handleDragEnd(ctx) {
 }
 
 function setOpen() {
-  axisY.value = windowHeight.value - sheetContent.value;
+  axisY.value = windowHeight.value - sheetContent.value + BOTTOM_PADDING;
   push("y", axisY.value, motionProperties, {
     ...selectedProp.value,
     duration: 100,
@@ -216,13 +215,17 @@ function setClose() {
   box-shadow: 0px -6px 24px rgb(10 15 45 / 9%);
   border-radius: 15px 15px 0 0;
 
-  position: absolute;
+  position: fixed;
   //   bottom: -300px;
   left: 0;
   right: 0;
   //   width: 100%;
+  z-index: 1001;
+  max-height: 95%;
   margin: 0;
   user-select: none;
+  padding-bottom: 500px;
+  -webkit-overflow-scrolling: touch;
 
   button.active {
     background-color: red;
